292aa5b68f0d88c6a853547c68c10081b00e8655
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2021-10-10 09:08:19 +0545

    JS Effect Accomplished

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 
-let svg=document.querySelectorAll('.svg');
-let ans=document.querySelectorAll('.answer');
+// let svg=document.querySelectorAll('.svg');
+// let ans=document.querySelectorAll('.answer');
 
-for(let i=0; i<svg.length; i++){
-    svg[i].addEventListener("click",(e)=>{
+// for(let i=0; i<svg.length; i++){
+//     svg[i].addEventListener("click",(e)=>{
 
-        if( ans[i].className ==="answer"){
-            ans[i].classList.toggle('active');
-            svg[i].classList.add("turn");
-        }else{
-            ans[i].classList.toggle('answer');
-            svg[i].classList.remove("turn");
-        }
+//         if( ans[i].className ==="answer"){
+//             ans[i].classList.toggle('active');
+//             svg[i].classList.toggle("turn");
+//         }else{
+//             ans[i].classList.toggle('answer');
+//             svg[i].classList.toggle("turn");
+//         }
         
-        // console.log(e.target);
+//         // console.log(e.target);
+//     });
+// }
+ 
+ 
+
+
+let faqs= document.querySelectorAll('.container');
+
+for(let i=0; i<faqs.length; i++){
+    faqs[i].addEventListener("click",(e)=>{
+       faqs[i].classList.toggle("active");
+       faqs[i].classList.toggle("container");
     });
 }
  
